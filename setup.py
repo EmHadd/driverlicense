f5c7f1e82fdc96e9eff91eca021e0057a1709f89
m-rau <m.rau@plan-net.com>
2019-10-05 16:50:04 +0200

    initial version

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "core4 @ git+https://github.com/plan-net/core4.git"
+        "core4 @ git+https://github.com/plan-net/core4.git",
+        "xlrd"
     ]
 )
