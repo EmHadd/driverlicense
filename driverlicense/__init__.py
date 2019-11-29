54060b8e4a9108b8541b0af105e98ec6225bb967
m-rau <m.rau@plan-net.com>
2019-11-29 10:54:45 +0100

    bump release-0.0.1

--- driverlicense/__init__.py
+++ driverlicense/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.0.0'
+__version__ = "0.0.1"
 __project__ = 'core4'
-__built__ = None
+__built__ = "2019-11-29 09:54:45.326000"
 
 name = 'driverlicense'
 title = 'driverlicense (core4 project)'
