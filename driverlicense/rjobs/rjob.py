63709feaa02f4ced3b37772b69aa7b05ef7b505f
emma <emmahaddi@gmail.com>
2019-12-04 15:09:06 +0100

    new R session script

--- driverlicense/rjobs/rjob.py
+++ driverlicense/rjobs/rjob.py
@@ -1,7 +1,8 @@
 from core4.queue.job import CoreJob
+from driverlicense.rjobs.rsession import get_rsession, as_rdf
 import pandas as pd
-from rpy2 import robjects as ro
-from rpy2.robjects import pandas2ri
+import unidecode
+import numpy as np
 import os
 
 import rpy2.robjects.packages as rpackages
@@ -15,28 +16,51 @@ class RJob(CoreJob):
     author = "eha"
 
     def execute(self):
-        pandas2ri.activate()
-        R = ro.r
-        df = pd.DataFrame({'x': [1,2,3,4,5],
-                           'y': [2,1,3,5,4]})
-        M = R.lm('y~x', data=df)
-        self.logger.info(R.summary(M).rx('coefficients'))
-        R.gc()
-        res = self.get_func()
-        print(res.shape)
+        self.data = self.config.driverlicense.collection.data
+        r_session = get_rsession()
+        self.func1(r_session)
+        self.logger.info('imported and executed R func1')
+        # self.func2(r_session)
+        # self.logger.info('imported and executed R func2')
 
-    def get_func(self):
+    def func1(self, r):
+        """
+        In this function the connection to mongoDB happens in
+        R with the help of mongolite package
+        :param r: r session with required libraries
+        :return:
+        """
         url = self.config.driverlicense['mongo_url']
-        db = 'driverlisence'
+        db = 'driverlicense'
         collection = 'xlsx'
-        r = ro.r
-        rpackages.importr('mongolite',lib_loc= LIB_LOC)
-        rpackages.importr('openssl', lib_loc=LIB_LOC)
         r.source('script1.R')
+        r.debug(r.func1(collection, db, url))
         res = r.func1(collection, db, url)
-
+        self.logger.info('Dataframe size is %s', res.shape)
         return res
 
+    def func2(self, r):
+        """
+        In this function R recieves the data as an R dataframe
+        object using the function pandas2ri.py2ri
+        :param r: r session with required libraries
+        :return:
+        """
+        cur = self.data.find()
+        df = pd.DataFrame(list(cur))
+        df = df.replace(np.nan, 0)
+        df['Monat'] = df['Monat'].dt.strftime(
+            '%Y-%m-%d')
+        df.columns = [self.remove_accents(c.replace("%"," percent")) for c in df.columns]
+        for col in df.columns:
+           df[col] = df[col].apply(self.remove_accents) if col in ['Grundgesamtheit', 'Titel', 'Analyse'] else df[col]
+        #df = py_to_r(df)
+        r.source('script1.R')
+        res = r.func2()
+        return res
+
+    def remove_accents(self, string):
+        return unidecode.unidecode(string)
 
 if __name__ == '__main__':
     from core4.queue.helper.functool import execute
--- driverlicense/rjobs/script1.R
+++ driverlicense/rjobs/script1.R
@@ -1,13 +1,14 @@
 
-library(mongolite)
+# library(mongolite)
 
 func1 <- function(collection, db, url){
-
+    logdebug('I am a silent child')
     # Connection to mongodb
     col <- mongo(collection , db, url)
-
+    loginfo('connected to database')
     # documents within collection are transformed into R dataframe
     df <- col$find()
+    loginfo('dataframe name is %s', 'dataframe')
 
     return(df)
     }
